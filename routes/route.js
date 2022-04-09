e320971a9ecba352003dd18e0c9128f4e4474555
Kaiwen Song <ksong@coretechs.com>
2022-04-09 13:07:05 -0400

    Route update

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/controller')
 
-router.get('/', controller.getData)
+router.get('/getDailyInfo', controller.getData)
 
 module.exports = router
