0f0a038fff85711faa2542f9503b324b4d315a23
Kaiwen Song <ksong@coretechs.com>
2022-04-10 03:08:37 -0400

    Bug Fixes

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ cron.schedule('* * * * *', async () => {
   })
 
   let wholeList = getWholeList.map(entry => entry.answer)
-  
+
   let bundledData = {}
   if (answer.includes("&")) {
     const champion = answer.split("&")[0];
@@ -76,10 +76,10 @@ cron.schedule('* * * * *', async () => {
     const abilityData = testData.data["data"][champion]["spells"][ability];
     const abilityDescription = abilityData.description.replaceAll(champion, "The Champion")
 
-    bundledData["category"] = "Champion Ability"
-    bundledData["answer"] = champion + " " + answer.split("&")[1]
+    bundledData["category"] = "Champion Ultimate"
+    bundledData["answer"] = champion + " " + answer.split("_")[1]
     bundledData["clues"] = [abilityData.name, abilityData["image"]["full"], abilityDescription]
-    bundledData["choices"] = wholeList.filter(entry => entry.includes("&")).map(entry => entry.replace("&", " "))
+    bundledData["choices"] = wholeList.filter(entry => entry.includes("_")).map(entry => entry.replace("_", " "))
   }
   else if (!isNaN(answer)){
     const testData = await axios.get("http://ddragon.leagueoflegends.com/cdn/12.6.1/data/en_US/item.json")
@@ -107,7 +107,7 @@ cron.schedule('* * * * *', async () => {
     bundledData["category"] = "Item"
     bundledData["answer"] = itemData["name"]
     bundledData["clues"] = [itemStats, buildPath, itemDescription]
-    bundledData["choices"] = wholeList.filter(entry => !isNaN(entry)).map(entry => entry)
+    bundledData["choices"] = wholeList.filter(entry => !isNaN(entry)).map(entry => testData.data["data"][entry]["name"])
   }
   else {
     const champion = answer;
@@ -126,7 +126,7 @@ cron.schedule('* * * * *', async () => {
     bundledData["category"] = "Champion"
     bundledData["answer"] = champion
     bundledData["clues"] = [trivia, "Passive: " + championData["passive"]["name"], championData["title"]]
-    bundledData["choices"] = wholeList.filter(entry => (isNaN(entry) && !entry.includes("&"))).map(entry => entry)
+    bundledData["choices"] = wholeList.filter(entry => (isNaN(entry) && !entry.includes("_")))
   }
 
   console.log(bundledData["choices"])
